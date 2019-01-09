0086ef5ec5e735b532e873488aad447fa14321f3
Jorge Morante <jorge@morante.eu>
2019-01-09 09:04:30 +0100

    cleanup and initialize bindings on background

--- scripts/send-input.sh
+++ scripts/send-input.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source $CURRENT_DIR/debug.sh
-
 tmux wait-for -L fingers-input
 
-log "[input] sending command '$1'"
 echo "$1" >> /tmp/fingers-command-queue
 
 tmux wait-for -U fingers-input
---
+++ scripts/setup-fingers-mode-bindings.sh
@@ -0,0 +1,29 @@
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function fingers_bind() {
+  local key="$1"
+  local command="$2"
+
+  tmux bind-key -Tfingers "$key" run-shell -b "$CURRENT_DIR/send-input.sh '$command'"
+}
+
+for char in {a..z}
+do
+  if [[ "$char" == "c" ]]; then
+    continue
+  fi
+
+  fingers_bind "$char" "hint:$char:main"
+  fingers_bind "$(echo "$char" | tr '[:lower:]' '[:upper:]')" "hint:$char:shift"
+  fingers_bind "C-$char" "hint:$char:ctrl"
+  fingers_bind "M-$char" "hint:$char:alt"
+done
+
+fingers_bind "C-c" "exit"
+fingers_bind "Escape" "exit"
+fingers_bind "q" "exit"
+
+fingers_bind "?" "toggle-help"
+fingers_bind "Space" "toggle-compact-mode"
+
+fingers_bind "Any" "continue"
